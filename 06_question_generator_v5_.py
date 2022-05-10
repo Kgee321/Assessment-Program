c3594bdfadd131eb22112043512c85fa4441a26a
Kgee321 <geek@middleton.school.nz>
2022-05-10 15:03:33 +1200

    Changes

--- 06_question_generator_v5_.py
+++ 06_question_generator_v5_.py
@@ -1,6 +1,6 @@
-"""Component 4 (Random Question) -- Easy mode -- Version 5
-I am using version 4 to use as my code because it uses less code
-Making the code so questions do not repeat themselves
+"""Component 4 (Random Question) -- Easy mode -- Version 4
+Using a range loop to check if user got the answer right instead of
+having the very long if statement to see if it is more efficient
 Written by Katelyn Gee
 Created 9/05/2022"""
 
@@ -11,7 +11,7 @@ score = 0
 
 # List of Maori words up to 10
 maori_numbers = ["Tahi", "Rua", "Toru", "Wha", "Rima", "Ono", "Whitu", "Waru", "Iwa", "Tekau"]
-other_list = []
+numbers_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 for number in range(5):
 
@@ -26,11 +26,15 @@ for number in range(5):
     question = int(input(f"What is {maori_choice} in English? (Answer using numbers) "))
 
     # Loop to check if user is correct
-    for num in range(len(maori_numbers) + len(other_list)):
-        new_num = num + 1
+    for num in range(10):
 
+        new_num = num + 1
+        print(maori_choice)
+        print(maori_numbers[num])
+        print(question)
+        print(numbers_list[num])
         # User got it right, loop breaks
-        if maori_choice == maori_numbers[num] and question == new_num:
+        if maori_choice == maori_numbers[num] and question == numbers_list[num]:
             score += 1
             answer = "right!"
             break
@@ -38,9 +42,9 @@ for number in range(5):
         else:
             answer = "wrong"
 
-    # Removes Maori numbers from list so questions does not repeat
-    other_list.append(maori_choice)
+    # Removing
     maori_numbers.remove(maori_choice)
+    numbers_list.remove(question)
 
     # Printing if user is right or wrong
     print(f"You got it {answer}")
